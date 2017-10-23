e8271355dc34cc877475f463208fcd9cbb666df9
Alex Creek <me@alexcreek.com>
2017-10-22 23:15:32 -0400

    add ubuntu version variable to repo

--- modules/profile/manifests/docker/repo.pp
+++ modules/profile/manifests/docker/repo.pp
@@ -4,7 +4,7 @@ class profile::docker::repo {
 
   apt::source { 'docker':
     location => 'https://download.docker.com/linux/ubuntu',
-    release  => 'zesty',
+    release  => $::os['distro']['codename'],
     repos    => 'stable',
     key      => {
       'id'     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
