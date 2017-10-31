86bfe78a07ab43c989134cf75258d8eb4213acb2
acreek <alex.creek@fireeye.com>
2017-10-30 23:04:30 -0400

    add firewall rules

--- modules/profile/manifests/openvpn_server.pp
+++ modules/profile/manifests/openvpn_server.pp
@@ -1,7 +1,6 @@
 #
 class profile::openvpn_server {
   openvpn::server { 'vpnserver':
-#    remote       => ['168.235.81.155'],
     topology     => 'subnet',
     country      => 'US',
     province     => 'CA',
@@ -9,13 +8,56 @@ class profile::openvpn_server {
     organization => 'Prestige Worldwide',
     email        => 'root@fuckoff.io',
     server       => '10.10.10.0 255.255.255.0',
+    push         => ['dhcp-option DNS 8.8.8.8'],
   }
+
+  Openvpn::Client {
+    remote_host => $::ipaddress,
+  }
+
   openvpn::client { 'client1':
-    server => 'vpnserver',
+    server      => 'vpnserver',
   }
+
   openvpn::client_specific_config { 'client1':
     server           => 'vpnserver',
     redirect_gateway => true,
     ifconfig         => '10.10.10.2 255.255.255.0',
+  }
+
+  # to allow saving rules
+  package { 'iptables-persistent':
+    ensure => installed,
+  }
+
+  firewall { '1 allow ssh':
+    dport  => 22,
+    proto  => tcp,
+    action => accept,
+  }
+
+  firewall { '2 forwarding from tun':
+    chain    => 'FORWARD',
+    action   => 'accept',
+    proto    => 'all',
+    outiface => 'eth0',
+    source   => '10.10.10.0/24',
+    state    => ['NEW'],
+  }
+
+  firewall { '3 forwarding in general':
+    chain  => 'FORWARD',
+    action => 'accept',
+    proto  => 'all',
+    state  => ['RELATED','ESTABLISHED'],
+  }
+
+  firewall { '4 snat for clients':
+    chain    => 'POSTROUTING',
+    jump     => 'MASQUERADE',
+    proto    => 'all',
+    outiface => 'eth0',
+    source   => '10.10.10.0/24',
+    table    => 'nat',
   }
 }
