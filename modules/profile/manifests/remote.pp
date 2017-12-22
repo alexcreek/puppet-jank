1bf84c31c38f0ea1931caf4a9eb9df144133c1db
Alex Creek <me@alexcreek.com>
2017-12-21 22:56:05 -0500

    add remote profile

---
+++ modules/profile/manifests/remote.pp
@@ -0,0 +1,24 @@
+#
+class profile::remote {
+  class { 'supervisord':
+    install_pip => true,
+  }
+  
+  Supervisord::Program {
+    autostart   => true,
+    autorestart => true,
+  }
+
+  supervisord::program { 'remote':
+    command             => '/usr/local/bin/flask run -h 0.0.0.0 -p 5000',
+    directory           => '/opt/remote',
+    program_environment => {
+      'FLASK_APP'   => 'remote.py',
+      'FLASK_DEBUG' => '0',
+    }
+  }
+
+  supervisord::program { 'websocketd':
+    command => '/usr/local/sbin/websocketd --port=9001 --dir=/opt/websockets',
+  }
+}
